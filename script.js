19e7951a5e8af4ac1d086d13380f5a44396a228a
psvetic <35137665+psvetic@users.noreply.github.com>
2021-02-08 14:58:48 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-let numValues = [["zero","one","two","three","four","five","six","seven","eight","nine","ten",
-                  "eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"],
-                 ["nula", "jedan", "dva", "tri", "četiri", "pet", "šest", "sedam", "osam", "devet", "deset",
-                  "jedanaest", "dvanaest", "trinaest", "četrnaest", "petnaest", "šesnaest", "sedamnaest", "osamnaest", "devetnaest"]];
+let numValues = [
+    ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+        "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
+    ],
+    ["nula", "jedan", "dva", "tri", "četiri", "pet", "šest", "sedam", "osam", "devet", "deset",
+        "jedanaest", "dvanaest", "trinaest", "četrnaest", "petnaest", "šesnaest", "sedamnaest", "osamnaest", "devetnaest"
+    ]
+];
 
-let deset = [["zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"],
-             ["nula", "deset", "dvadeset", "trideset", "četrdeset", "pedeset", "šezdeset", "sedamdeset", "osamdeset", "devedeset"]];
+let deset = [
+    ["zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"],
+    ["nula", "deset", "dvadeset", "trideset", "četrdeset", "pedeset", "šezdeset", "sedamdeset", "osamdeset", "devedeset"]
+];
 
 let numberToConvert = 0;
 let decimalPointPos = 0;
@@ -46,7 +52,7 @@ function getRandom() {
     numberToConvert = temp.toFixed(2);
 
     document.querySelector(".input-field").value = numberToConvert;
-  
+
     language = document.querySelector('input[name="lang"]:checked').value;
     if (language == "eng") {
         lang = 0;
@@ -60,14 +66,12 @@ function getRandom() {
     document.querySelector(".result").innerHTML = "Result: " + finalfinal;
 }
 
-function changeGender(number, word)
-{
-    if (number == 1 || number == 2 || number == 3 || number == 4)
-    {
-        switch(word) {
+function changeGender(number, word) {
+    if (number == 1 || number == 2 || number == 3 || number == 4) {
+        switch (word) {
             case 'k':
                 finalString.pop();
-            
+
                 if (number == 1) {
                     finalString.pop();
                     finalString.push("jedna", "kuna");
@@ -81,7 +85,7 @@ function changeGender(number, word)
                 break;
             case 'l':
                 finalString.pop();
-            
+
                 if (number == 1) {
                     finalString.pop();
                     finalString.push("jedna", "lipa");
@@ -95,7 +99,7 @@ function changeGender(number, word)
                 break;
             case 's':
                 finalString.pop();
-                
+
                 if (number == 1) {
                     finalString.pop();
                     finalString.push("jedna", "stotina");
@@ -127,13 +131,12 @@ function changeGender(number, word)
     }
 }
 
-function dictionary(number, position, lang)
-{
+function dictionary(number, position, lang) {
     console.log(number, position);
-    if (number == 0) {
+    if (number === '0') {
         return;
     }
-    switch(position) {
+    switch (position) {
         case 1:
             if (lang == 1) {
                 finalString.push("i");
@@ -141,7 +144,7 @@ function dictionary(number, position, lang)
             finalString.push(numValues[lang][number]);
             break;
         case 2:
-            if (number < 20 && number > 10) {
+            if (number < 20 && number >= 10) {
                 if (lang == 1) {
                     finalString.push("i");
                 }
@@ -150,7 +153,7 @@ function dictionary(number, position, lang)
                 if (lang == 1) {
                     finalString.push("i");
                 }
-                finalString.push(deset[lang][number/10]);
+                finalString.push(deset[lang][number / 10]);
             } else if (number > 0 && number < 10) {
                 finalString.push(deset[lang][number]);
             } else if (number == "00") {
@@ -158,7 +161,7 @@ function dictionary(number, position, lang)
             }
             break;
         case 5:
-            if (number < 20 && number > 10) {
+            if (number < 20 && number >= 10) {
                 finalString.push(numValues[lang][number]);
                 if (lang == 1) {
                     finalString.push("tisuća");
@@ -166,7 +169,7 @@ function dictionary(number, position, lang)
                     finalString.push("thousand");
                 }
             } else if (number > 19) {
-                finalString.push(deset[lang][number/10]);
+                finalString.push(deset[lang][number / 10]);
                 if (lang == 1) {
                     finalString.push("tisuća");
                 } else {
@@ -184,7 +187,7 @@ function dictionary(number, position, lang)
             break;
         case 3:
         case 6:
-            if (lang == 1){
+            if (lang == 1) {
                 finalString.push(numValues[lang][number], "stotina");
                 changeGender(number, 's');
             } else {
@@ -192,11 +195,11 @@ function dictionary(number, position, lang)
             }
             break;
         case 4:
-            if (lang == 1){
-                finalString.push(numValues[lang][number], "tisuća");
-                changeGender(number,'t');
+            if (lang == 1) {
+                finalString.push("i", numValues[lang][number], "tisuća");
+                changeGender(number, 't');
             } else {
-                finalString.push(numValues[lang][number], "thousand");
+                finalString.push("and", numValues[lang][number], "thousand");
             }
             break;
         default:
@@ -206,137 +209,138 @@ function dictionary(number, position, lang)
 
 function main() {
 
-// ako postoje lipe
-if (Math.floor(numberToConvert).toString() != numberToConvert.toString()) {
-    
-    numberAsString = numberToConvert.toString().split('');
+    // ako postoje lipe
+    if (Math.floor(numberToConvert).toString() != numberToConvert.toString()) {
 
-    for (let i=0; i < numberAsString.length; i++) {
-        if (isNaN(numberAsString[i])) {
-            decimalPointPos = i;
+        numberAsString = numberToConvert.toString().split('');
+
+        for (let i = 0; i < numberAsString.length; i++) {
+            if (isNaN(numberAsString[i])) {
+                decimalPointPos = i;
+            }
         }
+
+        change = numberAsString.splice(decimalPointPos + 1, 2);
+
+        numberAsString.pop();
+
+        numberToConvert = [...numberAsString, ".", ...change].join('');
+
     }
-    
-    change = numberAsString.splice(decimalPointPos + 1, 2);
-    
-    numberAsString.pop();
-
-    numberToConvert = [...numberAsString, ".", ...change].join('');
-
-}
-// number is int rather than float
-else {
-    numberAsString = numberToConvert.toString().split('');
-    change = ["0"];
-}
-
-numberLength = numberAsString.length;
-
-// number on second place join with next one
-if (numberAsString[numberLength-2] == 1 || numberAsString[numberLength-1] == 0)
-{
-    numberAsString[numberLength-2] = numberAsString[numberLength-2] + numberAsString[numberLength-1];
-    numberAsString.pop();
-}
-// number in fifth place join with next (eg. sixteen)
-if (numberAsString[numberLength-5] == 1 || numberAsString[numberLength-4] == 0)
-{
-    numberAsString[numberLength-5] = numberAsString[numberLength-5] + numberAsString[numberLength-4];
-    numberAsString.splice(numberLength-4, 1);
-
-    if (numberLength == 5) {
-        for (let i=0; i < 1; i++) {
-            dictionary(numberAsString[i], numberLength-i, lang);
-        }
-        for (let i=1; i < numberAsString.length; i++) {
-            dictionary(numberAsString[i], numberLength-i-1, lang);
-        }
-    } else {
-        for (let i=0; i < 2; i++) {
-            dictionary(numberAsString[i], numberLength-i, lang);
-        }
-        for (let i=2; i < numberAsString.length; i++) {
-            dictionary(numberAsString[i], numberLength-i-1, lang);
-        }
-    }
-
-    if (lang == 1) {
-        finalString.push("kuna");
-        changeGender(numberAsString[numberAsString.length-1], 'k');
-    } else {
-        finalString.push("dollars");
-    }
-
-} else {
-    if(numberToConvert > 19) {
-        for (let i=0; i < numberAsString.length; i++) {
-            dictionary(numberAsString[i], numberLength-i, lang);
-        }
-        if (lang == 1) {
-            finalString.push("kuna");
-            changeGender(numberAsString[numberAsString.length-1], 'k');
-        } else {
-            finalString.push("dollars");
-        }
-    }
+    // number is int rather than float
     else {
-        finalString.push(numValues[lang][numberToConvert]);
+        numberAsString = numberToConvert.toString().split('');
+        change = ["0"];
+    }
+
+    numberLength = numberAsString.length;
+
+    // number on second place join with next one
+    if (numberAsString[numberLength - 2] == 1 || numberAsString[numberLength - 1] == 0) {
+        numberAsString[numberLength - 2] = numberAsString[numberLength - 2] + numberAsString[numberLength - 1];
+        numberAsString.pop();
+    }
+    // number in fifth place join with next (eg. sixteen)
+    if (numberAsString[numberLength - 5] == 1 || numberAsString[numberLength - 4] == 0) {
+        numberAsString[numberLength - 5] = numberAsString[numberLength - 5] + numberAsString[numberLength - 4];
+        numberAsString.splice(numberLength - 4, 1);
+
+        if (numberLength == 5) {
+            for (let i = 0; i < 1; i++) {
+                dictionary(numberAsString[i], numberLength - i, lang);
+            }
+            for (let i = 1; i < numberAsString.length; i++) {
+                dictionary(numberAsString[i], numberLength - i - 1, lang);
+            }
+        } else {
+            for (let i = 0; i < 2; i++) {
+                dictionary(numberAsString[i], numberLength - i, lang);
+            }
+            for (let i = 2; i < numberAsString.length; i++) {
+                dictionary(numberAsString[i], numberLength - i - 1, lang);
+            }
+        }
+
         if (lang == 1) {
             finalString.push("kuna");
+            changeGender(numberAsString[numberAsString.length - 1], 'k');
         } else {
             finalString.push("dollars");
         }
-    }
-}
 
-// modifikacije lipa
-
-// if change is "00"
-if (change[0] == 0 && change[1] == 0) {
-    change.pop();
-}
-// numbers from 11 to 19
-if (change[0] == 1 && change.length > 1)
-{
-    change[0] = change[0] + change[1];
-    change.pop();
-
-    if (lang == 1) {
-        finalString.push("i", numValues[lang][change[0]], "lipa");
     } else {
-        finalString.push("and", numValues[lang][change[0]], "cents");
+        if (numberToConvert > 19) {
+            for (let i = 0; i < numberAsString.length; i++) {
+                dictionary(numberAsString[i], numberLength - i, lang);
+            }
+            if (lang == 1) {
+                finalString.push("kuna");
+                changeGender(numberAsString[numberAsString.length - 1], 'k');
+            } else {
+                finalString.push("dollars");
+            }
+        } else {
+            finalString.push(numValues[lang][numberToConvert]);
+            if (lang == 1) {
+                finalString.push("kuna");
+            } else {
+                finalString.push("dollars");
+            }
+        }
     }
-// numbers 10, 20, ... , 90
-} else if (change.length == 1) {
-    if (lang == 1) {
-        finalString.push("i", deset[lang][change[0]], "lipa");
+
+    // modifikacije lipa
+
+    // if change is "00"
+    if (change[0] == 0 && change[1] == 0) {
+        change.pop();
+    }
+    // numbers from 11 to 19
+    if (change[0] == 1 && change.length > 1) {
+        change[0] = change[0] + change[1];
+        change.pop();
+
+        if (lang == 1) {
+            finalString.push("i", numValues[lang][change[0]], "lipa");
+        } else {
+            finalString.push("and", numValues[lang][change[0]], "cents");
+        }
+        // numbers 10, 20, ... , 90
+    } else if (change.length == 1) {
+        if (lang == 1) {
+            finalString.push("i", deset[lang][change[0]], "lipa");
+        } else {
+            finalString.push("and", deset[lang][change[0]], "cents");
+        }
+        // all the other numbers
     } else {
-        finalString.push("and", deset[lang][change[0]], "cents");
+        // numbers from 1 to 9
+        if (change[0] == 0 && change.length > 1) {
+
+        } else {
+            if (lang == 1) {
+                finalString.push("i");
+            } else {
+                finalString.push("and");
+            }
+        }
+
+        dictionary(change[0], 2, lang);
+        dictionary(change[1], 1, lang);
+
+        if (lang == 1) {
+            finalString.push("lipa");
+            changeGender(change[1], 'l');
+        } else {
+            finalString.push("cents");
+        }
+
     }
-// all the other numbers
-} else {
-    if (lang == 1) {
-        finalString.push("i");
-    } else {
-        finalString.push("and");
-    }
 
-    dictionary(change[0], 2, lang);
-    dictionary(change[1], 1, lang);
+    console.log(change);
+    console.log(numberAsString);
 
-    if (lang == 1) {
-        finalString.push("lipa");
-        changeGender(change[1], 'l');
-    } else {
-        finalString.push("cents");
-    }
-
-}
-
-console.log(change);
-console.log(numberAsString);
-
-finalfinal = finalString.join(' ');
-console.log("--->", finalfinal);
+    finalfinal = finalString.join(' ');
+    console.log("--->", finalfinal);
 
 }
